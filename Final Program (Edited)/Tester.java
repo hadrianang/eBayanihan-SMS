fb0dd494428e3b365bde53f61a121fde7dd7e5e4
Hadrian Ang <hadrianang@outlook.com>
2015-06-29 10:37:03 +0800

    Fixed not problem

--- Final Program (Edited)/Tester.java
+++ Final Program (Edited)/Tester.java
@@ -15,12 +15,19 @@ public class Tester
 			if(temp==null) break; 
 			temp = temp.toLowerCase();
 			String[] arr = ws.wordBreak(temp.trim(),3); 
+			boolean not = false; 
 			for(int i=0; i<arr.length; i++)
 			{
 				if(i!=0)
 				{
-					if(arr[i].equals("city") || arr[i].equals("not")) System.out.print(" ");
+					if(arr[i].equals("city") || not) 
+					{
+						System.out.print(" ");
+						not = false; 
+					}
 					else System.out.print(",");
+					
+					if(arr[i].equals("not")) not = true; 
 				}
 				System.out.print(arr[i]); 
 			}
